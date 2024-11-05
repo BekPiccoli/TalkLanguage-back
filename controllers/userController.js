77124595a1bcfb8a5119fccd1cfa4c28c0c820f2
higorwilvert <higorwilvert16@gmail.com>
2024-11-05 16:31:36 -0300

    Criado função para atualizar os dados selecioando pelo usuario

--- controllers/userController.js
+++ controllers/userController.js
@@ -116,4 +116,42 @@ export async function remove(req, res) {
       res.status(500).json({ message: "Server Error" });
     }
   }
-  
+
+
+  export async function updateUserPreferences(req, res) {
+    console.log("CHEGOU NA ATUALIOZXAO")
+  try {
+    const { userId } = req.params;  // Assumindo que o ID do usuário vem nos parâmetros da URL
+    const { primaryLanguage, level, purpose } = req.body;  // Dados para atualizar no usuário
+
+    console.log("chegouuuuuuuu no addUserLanguage");
+    console.log(userId);
+    console.log(primaryLanguage);
+    console.log(level);
+    console.log(purpose);
+    
+    // Verifica se o usuário existe
+    const user = await prisma.user.findUnique({ where: { id: userId } });
+    if (!user) {
+      return res.status(404).json({ message: "Usuário não encontrado" });
+    }
+
+    // Atualiza os campos do usuário
+    const updatedUser = await prisma.user.update({
+      where: { id: userId },
+      data: {
+        primaryLanguage,
+        level,
+        purpose,
+      },
+    });
+
+    res.status(200).json({
+      message: "Preferências atualizadas com sucesso",
+      user: updatedUser,
+    });
+  } catch (error) {
+    console.error("Erro ao atualizar preferências do usuário:", error);
+    res.status(500).json({ message: "Erro ao atualizar usuário" });
+  }
+}
